983f0929011ced29ebacc5e5dce5670b0a9b7fcd
p <p>
2020-12-25 16:02:16 -0800

    periodic commit

--- config.h
+++ config.h
@@ -4,15 +4,15 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 10;       /*horiz inner gap between windows*/
+static const unsigned int gappiv    = 10;       /*vert inner gap between windows*/
+static const unsigned int gappoh    = 10;       /*horiz outer gap between windows and screen edge*/
+static const unsigned int gappov    = 10;       /*vert outer gap between windows and screen edge*/
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Sauce Code Pro Nerd Font Mono:style=medium:size=12" };
-static const char dmenufont[]       = "Sauce Code Pro Font Mono:style=medium:size=12";
+static const char *fonts[]          = { "TerminessTTF Nerd Font Mono:style=medium:size=12" };
+static const char dmenufont[]       = "TerminessTTF Nerd Font Mono:style=medium:size=12";
 static char normbgcolor[]           = "#1d1b18";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#cfc5b4";
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
@@ -161,4 +161,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
